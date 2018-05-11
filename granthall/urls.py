586776ebf5398871a1f86d377b8eaaf716211eba
Akademiks139 <31316720+Akademiks139@users.noreply.github.com>
2018-05-11 08:42:56 -0400

    Update urls.py

--- granthall/urls.py
+++ granthall/urls.py
@@ -6,9 +6,6 @@ app_name='granthall'
 
 urlpatterns = [
 	path('', views.index, name='home'),
-	#path('dataentry', views.dataEntry, name='dataEntry'),
-	#path('dataquery', views.dataQuery, name='dataQuery'),
-	#path('help', views.help, name='help'),
 	path('additem', views.additem, name='additem'),
 	path('showitems', views.showitems, name='showitems'),
 ]
